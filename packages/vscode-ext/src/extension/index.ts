157eded7e45237060a2ad30e73140ee12a812bee
makoto.kume <makoto.kume07@gmail.com>
2024-06-20 07:58:01 +0900

    VSCode拡張でポップアップの位置が不正になる問題を修正
    https://github.com/floating-ui/floating-ui/issues/2738

--- packages/vscode-ext/src/extension/index.ts
+++ packages/vscode-ext/src/extension/index.ts
@@ -20,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Webview</title>
-    <script defer src="${scriptUrl}"></script>
+    <script defer type="module" src="${scriptUrl}"></script>
     </head>
     <body><div id="root" /></body>
     </html>
